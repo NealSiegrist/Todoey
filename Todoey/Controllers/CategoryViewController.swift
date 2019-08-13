03251ef68b6781cc192297a6a66704361466a75a
NealSiegrist <nmsiegrist@dons.usfca.udu>
2019-08-12 18:17:00 -0700

    Add complete Core Date strategy and category and search screen.

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,107 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Neal Siegrist on 8/11/19.
+//  Copyright © 2019 Neal Siegrist. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    var categories = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadCategories()
+        
+    }
+
+    //MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categories[indexPath.row].name
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let destinationVC = segue.destination as! TodoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
+        
+    }
+    
+    //MARK: - Data Manipulation Methods
+    
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func saveCategories() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    
+    //MARK: - Add New Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+    
+        let action = UIAlertAction(title: "Add", style: .default) { (alert) in
+            let category = Category(context: self.context)
+            
+            category.name = textField.text!
+            self.categories.append(category)
+            
+            self.saveCategories()
+        
+        }
+        
+        alert.addAction(action)
+        alert.addTextField { (field) in
+            field.placeholder = "Add new category"
+            textField = field
+        }
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+}
